0ef417dfa26c2a7e9e42756db461ebd0dff767d4
ヘイスアヤン <park.ahyung52@gmail.com>
2020-06-26 19:01:18 +0900

    added files for firebase

---
+++ pages/inspire.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;{{ firstUser.name }}</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+interface User {
+  name: string
+}
+
+@Component
+export default class TestClass extends Vue {
+  mounted() {
+    this.testFunc()
+  }
+
+  firstUser: User = {
+    name: '',
+  }
+
+  testFunc() {
+    this.firstUser.name = 'firstUser'
+  }
+}
+</script>
